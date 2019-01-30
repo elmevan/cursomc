e9221ee5a877e76b45db7f95dee7ce1c7675fd44
Vander Ribeiro Elme <vander@elme.com.br>
2019-01-29 22:31:34 -0200

    V2

--- src/main/java/br/com/elme/domain/Categoria.java
+++ src/main/java/br/com/elme/domain/Categoria.java
@@ -1,9 +1,18 @@
 package br.com.elme.domain;
 
 import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
 
+
+@Entity
 public class Categoria implements Serializable{
 	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 	
